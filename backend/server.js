c35ab6d9ebfa20a7bbfe0bf238e6cbabf0341eba
luttinno <luka.grdzelishvili1994@gmail.com>
2026-04-17 21:34:38 +0400

    allow local and vercel frontend
    Signed-off-by: luttinno <luka.grdzelishvili1994@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import authRouter from "./routes/auth.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+// allow both local frontend and deployed frontend
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mern-auth-five-mu.vercel.app",
+];
+
 const app = express();
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
